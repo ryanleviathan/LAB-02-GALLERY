2f45a3bcf61cf4b0c3fa96a5eb7b4c1c31daf3ec
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-27 14:21:23 -0700

    added header, created js files for buildout

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>
+                    This is my cool website to look at (<em>and sort through</em>) images of animals with horns!
+                </h1>
+                <h3>
+                    I implore you to explore! Maybe you'll see some animals you've never seen before.
+                </h3>
+            </header>    
+        )
+    }
+}
